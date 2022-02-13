8de5637d95c9f18a7f22089366fc067fdad5933e
Zachary Chandler <chandlerzach@seattleu.edu>
2022-02-12 18:45:14 -0800

    reorganized directory to support future distribution

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ import setuptools
 with open("README.md", "r") as fh:
   long_description = fh.read()
 
+requirements = ["paho-mqtt"]
+
 setuptools.setup(
     name="mqtt_chat"
-  , install_requires=["paho-mqtt"]
+  , install_requires=requirements
   , version="0.1"
   , author="Zachary Chandler"
-  , author_email="chandlerzach@seattleu.edu"
-  , description="goofy mqtt chat client"
+  , author_email="zachary.c.me@gmail.com"
+  , description="light weight mqtt chat client"
+  , license = "MIT"
   , long_description = long_description
   , long_description_content_type="text/markdown"
-  , url="https://github.com/pypa/sampleproject"
+  , url="https://github.com/burnish3d/mqtt_chat"
   , packages=setuptools.find_packages()
   , classifiers=[
         "Programming Language :: Python :: 3"
